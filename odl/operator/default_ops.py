12760e2399b189010e912c454ff13d18f029b25f
Holger Kohr <h.kohr@cwi.nl>
2016-11-25 23:45:18 +0100

    ENH: add base classes for tensors and ufuncs and rename stuff

--- odl/operator/default_ops.py
+++ odl/operator/default_ops.py
@@ -894,7 +894,7 @@ class RealPart(Operator):
 
         Parameters
         ----------
-        space : `FnBase`
+        space : `TensorSpace`
             Space which real part should be taken, needs to implement
             ``space.real_space``.
 
@@ -914,7 +914,7 @@ class RealPart(Operator):
         >>> op([1, 2, 3])
         rn(3).element([1.0, 2.0, 3.0])
 
-        The operator also works on other `FnBase` spaces such as
+        The operator also works on other `TensorSpace` spaces such as
         `DiscreteLp` spaces:
 
         >>> r3 = odl.uniform_discr(0, 1, 3, dtype=complex)
@@ -1007,7 +1007,7 @@ class ImagPart(Operator):
 
         Parameters
         ----------
-        space : `FnBase`
+        space : `TensorSpace`
             Space which imaginary part should be taken, needs to implement
             ``space.real_space``.
 
@@ -1115,7 +1115,7 @@ class ComplexEmbedding(Operator):
 
         Parameters
         ----------
-        space : `FnBase`
+        space : `TensorSpace`
             Space which real part should be taken, needs to implement
             ``space.complex_space``.
         scalar : ``space.complex_space.field`` element, optional
